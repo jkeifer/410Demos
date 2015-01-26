6e605953322f9b8d5ecc298ba4d0ac9f318e2a5e
jkeifer <jarrettkeifer@gmail.com>
2015-01-26 15:39:56 -0800

    Demo 2

--- Demo1/demo1.py
+++ Demo1/demo1.py
@@ -1,10 +1,10 @@
 # **********************************************************************
-# 
+#
 # NAME: Your Name
 # DATE: date
 # CLASS: GEOG410
 # ASSIGNMENT: Lab #
-# 
+#
 # DESCRIPTION: describe the script, what it does, how it does it,
 #       and any other important information
 #
@@ -65,31 +65,40 @@ AT_RISK_BUILDINGS = "AtRiskBuildings"
 def create_gdb(location, name, overwrite=False):
     """
     """
-    # capture current overwrite setting    
+    # capture current overwrite setting
     overwritesetting = env.overwriteOutput
     # set env overwrite to match argument
     env.overwriteOutput = overwrite
-    
+
     if not name.endswith(GDB_EXT):
         name += GDB_EXT
-    
+
     geodatabase = os.path.join(location, name)
-    
+
     # try to create the gdb
     try:
         arcpy.CreateFileGDB_management(location, name)
     finally:
         # set overwrite back to previous setting
         env.overwriteOutput = overwritesetting
-        
+
     return geodatabase
-        
+
 
 def create_fds(gdbpath, fdsname, spatialref):
-    """
+    """Creates a feature dataset with the name supplied by fdsname
+    in the geodatabase given by gdbpath. The feature dataset will
+    have the spatial reference defeined by spatialref, which is an
+    arcpy SpatialReference object passed into the function.
+
+    If a fds already exists with the same name, this function will
+    simply do nothing.
+
+    This function returns the full path to the fds inside the
+    geodatabase.
     """
     fds = os.path.join(gdbpath, fdsname)
-    
+
     if not arcpy.Exists(fds):
         arcpy.CreateFeatureDataset_management(gdbpath, fdsname, spatialref)
 
@@ -101,8 +110,8 @@ def import_shapefiles_in_directory(directory, gdb_path, fds_name=None):
     """
     if fds_name:
         gdb_path = os.path.join(gdb_path, fds_name)
-    
-    
+
+
     for shapefile in get_shapefiles_in_directory(directory):
         shapefilename = os.path.splitext(os.path.basename(shapefile))[0]
 
@@ -128,14 +137,14 @@ def check_for_required_fcs(location, fcslist):
     """
     """
     errors = []
-    
+
     for fc in fcslist:
         if not arcpy.Exists(os.path.join(location, fc)):
             errors.append("Missing required feature class {} in {}.".format(fc, location))
-            
+
     return errors
-    
-    
+
+
 def build_zoning_query(zoningfc):
     """
     """
@@ -144,7 +153,7 @@ def build_zoning_query(zoningfc):
         query += "{} = '{}' OR ".format(arcpy.AddFieldDelimiters(zoningfc, ZONING_CLASS_FIELD),
                                         zoneclass)
     return query[:-4]
-      
+
 
 # ********** MAIN **********
 
@@ -152,16 +161,16 @@ def main():
     # create file geodatabase
     print ">Creating geodatabase {} in {}".format(GDB_NAME, DATA_DIR)
     fgdb = create_gdb(DATA_DIR, GDB_NAME, overwrite=True)
-    
+
     # create feature datasets in geodatabase
     print ">Creating feature datasets in geodatabase"
     fds_data = create_fds(fgdb, FDS_DATA_NAME, CRS)
     fds_results = create_fds(fgdb, FDS_RESULTS_NAME, CRS)
-    
+
     # copy shapefiles into data fds
     print ">Copying shapefiles into {}".format(fds_data)
     import_shapefiles_in_directory(DATA_DIR, fgdb, fds_name=FDS_DATA_NAME)
-    
+
     # make sure all necessary files are present
     print ">Validating copied FCs"
     missing_fcs = check_for_required_fcs(fds_data, REQUIRED_FCS)
@@ -169,7 +178,7 @@ def main():
         for fc in missing_fcs:
             print fc
         raise Exception("Required feature class(es) missing. Aborting.")
-        
+
     # find all large trees (>= analysis height)
     print ">Finding all large trees"
     trees = os.path.join(fds_data, TREES)
@@ -181,47 +190,47 @@ def main():
     # uncomment to keep buffers by re-setting "constant" to results fds
     #INTERMEDIATE_WORKSPACE = fds_results
 
-    # buffer trees by tree height 
-    print ">Buffering trees to find fall radius"   
+    # buffer trees by tree height
+    print ">Buffering trees to find fall radius"
     treebuffers = arcpy.Buffer_analysis(lrgtrees_lyr,
                                         os.path.join(INTERMEDIATE_WORKSPACE, "treebuff"),
                                         TREE_BUFF_FIELD)
-                                     
+
     # find resi zones
     print ">Finding residential zones"
     zoning = os.path.join(fds_data, ZONING)
     zoningquery = build_zoning_query(zoning)
     zoning_lyr = arcpy.MakeFeatureLayer_management(zoning, "zonelyr")
-    # can also use select by attributes with a feature layer    
+    # can also use select by attributes with a feature layer
     arcpy.SelectLayerByAttribute_management(zoning_lyr,
                                             where_clause=zoningquery)
-    
+
     # create buildings lyr
     print ">Creating buildings feature layer"
     buildings = os.path.join(fds_data, BUILDINGS)
     buildings_lyr =  arcpy.MakeFeatureLayer_management(buildings, "buildingslyr")
-    
+
     # find buildings in wanted zones
     print ">Finding buildings in wanted zones"
     arcpy.SelectLayerByLocation_management(buildings_lyr,
                                            "HAVE_THEIR_CENTER_IN",
                                            zoning_lyr)
-                                       
+
     # find buildings inside tree fall radius
     print ">Finding buildings inside tree fall radius"
     arcpy.SelectLayerByLocation_management(buildings_lyr,
                                            "INTERSECT",
                                            treebuffers,
                                            selection_type="SUBSET_SELECTION")
-                                           
+
     if int(arcpy.GetCount_management(buildings_lyr).getOutput(0)):
         outputfc = os.path.join(fds_results, AT_RISK_BUILDINGS)
         arcpy.CopyFeatures_management(buildings_lyr,
                                       outputfc)
-        print "\nFound at-risk buildings; features written to {}".format(outputfc)     
+        print "\nFound at-risk buildings; features written to {}".format(outputfc)
     else:
         print "\nNo buildings at risk."
-        
+
     return 0
 
 
